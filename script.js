170a63bc403aad2e4c8514df45b5d0bde5bceeaf
Agustin Colaci <agustincolaci@gmail.com>
2020-09-14 19:20:44 -0300

    Desafío nro. 4 - clase 6.

---
+++ script.js
@@ -0,0 +1,11 @@
+// Incorporar Arrays.
+
+
+var primerSemestre = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio'];
+var segundoSemestre = ['Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'];
+
+var year = primerSemestre.concat(segundoSemestre);
+
+console.log(year);
+
+
